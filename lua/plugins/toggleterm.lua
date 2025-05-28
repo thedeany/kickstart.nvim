7679f59b0ebd95ca2a3e1d553a90c6b9404bd61b
Brandon Dean <bdean@perq.com>
2025-05-28 10:52:20 -0400

    Change lazygit to use lazygit.nvim instead of opening in ToggleTerm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,14 +4,14 @@ return {
     version = '*',
     config = function()
       require('toggleterm').setup()
-      local Terminal = require('toggleterm.terminal').Terminal
-      local lazygit = Terminal:new { cmd = 'lazygit', direction = 'float' }
-
-      function _lazygit_toggle()
-        lazygit:toggle()
-      end
-
-      vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+      -- local Terminal = require('toggleterm.terminal').Terminal
+      -- local lazygit = Terminal:new { cmd = 'lazygit', direction = 'float' }
+      --
+      -- function _lazygit_toggle()
+      --   lazygit:toggle()
+      -- end
+      --
+      -- vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
     end,
   },
 }
